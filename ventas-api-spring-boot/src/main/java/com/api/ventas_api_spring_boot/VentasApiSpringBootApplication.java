bb944dfe3f301a3e74501ed498f4199e4f0fdf04
Matias Mercado <166638575+Eseekaa@users.noreply.github.com>
2025-06-06 02:44:26 -0400

    Add files via upload

---
+++ ventas-api-spring-boot/src/main/java/com/api/ventas_api_spring_boot/VentasApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.ventas_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasApiSpringBootApplication.class, args);
+	}
+
+}
